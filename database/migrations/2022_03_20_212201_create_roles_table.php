c890fd0c84109cd983ff56cb85fae2c0bc4b5afb
BadreddineZatout <hb_zatout@esi.dz>
2022-04-18 17:23:34 +0100

    fix role table

--- database/migrations/2022_03_20_212201_create_roles_table.php
+++ database/migrations/2022_03_20_212201_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->enum('name', ['student', 'decider', 'agent', 'admin', 'driver']);
+            $table->string('name');
             $table->timestamps();
         });
     }
