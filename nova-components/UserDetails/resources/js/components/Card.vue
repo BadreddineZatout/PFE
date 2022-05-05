52d7f6cd0d2bb1b8685980758ac95a39950855a5
BadreddineZatout <hb_zatout@esi.dz>
2022-05-05 15:44:43 +0100

    user details card init

---
+++ nova-components/UserDetails/resources/js/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="
+      flex flex-col
+      items-center
+      justify-center
+      shadow-lg
+      bg-blue-600
+      rounded-lg
+    "
+  >
+    <div class="px-3 py-3">
+      <h1 class="text-center text-3xl text-80 font-light">User Details</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "card",
+
+    // The following props are only available on resource detail cards...
+    // 'resource',
+    // 'resourceId',
+    // 'resourceName',
+  ],
+
+  mounted() {
+    //
+  },
+};
+</script>
