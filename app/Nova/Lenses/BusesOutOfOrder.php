8347656b0033ec56131519f91ba21d3dafb6897f
BadreddineZatout <hb_zatout@esi.dz>
2022-05-09 14:11:32 +0100

    bus resource lenses

---
+++ app/Nova/Lenses/BusesOutOfOrder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Nova\Lenses;
+
+use App\Nova\Metrics\BusHorsService;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Lenses\Lens;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Http\Requests\LensRequest;
+
+class BusesOutOfOrder extends Lens
+{
+    /**
+     * Get the query builder / paginator for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\LensRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return mixed
+     */
+    public static function query(LensRequest $request, $query)
+    {
+        return $request->withOrdering($request->withFilters(
+            $query->where('in_service', false)
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make('matricule'),
+            BelongsTo::make('establishment'),
+        ];
+    }
+
+    /**
+     * Get the cards available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [
+            new BusHorsService()
+        ];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return parent::actions($request);
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'buses-out-of-order';
+    }
+}
