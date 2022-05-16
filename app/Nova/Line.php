c46b84a38ab88f2ac34a25ca0cd69e10693b5035
BadreddineZatout <hb_zatout@esi.dz>
2022-05-16 11:44:18 +0100

    validation for line resource

--- app/Nova/Line.php
+++ app/Nova/Line.php
@@ -2,16 +2,18 @@
 
 namespace App\Nova;
 
-use App\Nova\Filters\BusEstablishment;
-use App\Nova\Filters\LineEstablishment;
+use App\Rules\MinBusNumber;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Number;
-use Laraning\NovaTimeField\TimeField;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\BelongsToMany;
+use App\Rules\MinRotationNumber;
 use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\BelongsTo;
+use Laraning\NovaTimeField\TimeField;
+use App\Nova\Filters\BusEstablishment;
+use Laravel\Nova\Fields\BelongsToMany;
+use App\Nova\Filters\LineEstablishment;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Titasgailius\SearchRelations\SearchesRelations;
 
@@ -114,13 +116,21 @@ class Line extends Resource
     {
         return [
             ID::make(__('ID'), 'id')->sortable(),
-            BelongsTo::make('plan'),
-            Text::make('name'),
-            Text::make('arab name', 'name_arabe')->hideFromIndex(),
-            TimeField::make('start time', 'start_time'),
-            TimeField::make('end time', 'end_time'),
-            Number::make('Rotations Number', 'rotations_number'),
-            Number::make('bus number', 'bus_number'),
+            BelongsTo::make('plan')
+                ->rules('required'),
+            Text::make('name')
+                ->rules('required', 'max:255'),
+            Text::make('arab name', 'name_arabe')
+                ->hideFromIndex()
+                ->rules('required', 'max:255'),
+            TimeField::make('start time', 'start_time')
+                ->rules('required'),
+            TimeField::make('end time', 'end_time')
+                ->rules('required'),
+            Number::make('Rotations Number', 'rotations_number')
+                ->rules('required', new MinRotationNumber),
+            Number::make('bus number', 'bus_number')
+                ->rules('required', new MinBusNumber),
             Boolean::make('aller retour', 'aller_retour'),
             BelongsToMany::make('stops')
                 ->fields(function ($request, $relatedModel) {
