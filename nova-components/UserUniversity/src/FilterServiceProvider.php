ab300b3855bcca7c3a3f975278bd7110d1247318
BadreddineZatout <hb_zatout@esi.dz>
2022-05-18 12:11:40 +0100

    wip

---
+++ nova-components/UserUniversity/src/FilterServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Badi\UserUniversity;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::script('user-university', __DIR__.'/../dist/js/filter.js');
+            Nova::style('user-university', __DIR__.'/../dist/css/filter.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
