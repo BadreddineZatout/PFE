494869c94f7da01d3bf881fb84f9d6c31e005f67
BadreddineZatout <hb_zatout@esi.dz>
2022-05-22 23:04:42 +0100

    food reservation test

---
+++ tests/Feature/FoodReservationResourceTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Role;
+use App\Models\User;
+use App\Nova\Filters\MealType;
+use App\Nova\Metrics\Leftovers;
+use NovaTesting\NovaAssertions;
+use App\Nova\Metrics\PreparedMeal;
+use App\Nova\Metrics\ConsumedByDay;
+use App\Nova\Metrics\ConsumedMeals;
+use App\Nova\Metrics\LeftoverByDay;
+use Illuminate\Support\Facades\Auth;
+use App\Nova\Metrics\ReservationsByDay;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FoodReservationResourceTest extends TestCase
+{
+    use NovaAssertions;
+
+    public function test_ReservationResourceIndex()
+    {
+        $user = User::findOrFail(1);
+        Auth::login($user);
+        $response = $this->novaIndex('reservations');
+
+        $response->assertStatus(200);
+
+        $response->assertCardCount(7);
+        $response->assertCardsInclude(PreparedMeal::class);
+        $response->assertCardsInclude(ReservationsByDay::class);
+        $response->assertCardsInclude(ConsumedMeals::class);
+        $response->assertCardsInclude(Leftovers::class);
+        $response->assertCardsInclude(ConsumedByDay::class);
+        $response->assertCardsInclude(LeftoverByDay::class);
+
+        $response = $this->novaIndex('reservations', [
+            MealType::class => 'breakfast'
+        ]);
+
+        $response->assertResourceCount(2);
+    }
+
+    public function test_ReservationResourceIndexForDecider()
+    {
+        $user = User::findOrFail(1);
+        $user->role_id = Role::DECIDER;
+        Auth::login($user);
+        $response = $this->novaIndex('reservations');
+
+        $response->assertStatus(200);
+        $response->assertCardCount(7);
+        $response->assertCardsInclude(PreparedMeal::class);
+        $response->assertCardsInclude(ReservationsByDay::class);
+        $response->assertCardsInclude(ConsumedMeals::class);
+        $response->assertCardsInclude(Leftovers::class);
+        $response->assertCardsInclude(ConsumedByDay::class);
+        $response->assertCardsInclude(LeftoverByDay::class);
+    }
+}
