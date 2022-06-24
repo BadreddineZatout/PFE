bb5bc5ced612005eb4e74ff50adf418d745b78d2
BadreddineZatout <hb_zatout@esi.dz>
2022-06-24 14:12:37 +0100

    Taken Equipment Resource

--- app/Models/TakenEquipment.php
+++ app/Models/TakenEquipment.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class TakenEquipment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['resident_id', 'equipment_id', 'quantity', 'take_date', 'return_date'];
+
+    protected $casts = [
+        'take_date' => 'date',
+        'return_date' => 'date'
+    ];
+
+    public function getNameAttribute()
+    {
+        return $this->equipment->name . ' / ' . $this->resident->user->name .
+            ' - ' . $this->resident->establishment->name_fr . ' - ' . $this->resident->structure->name .
+            ' - ' . $this->resident->place->name;
+    }
+
+    public function resident()
+    {
+        return $this->belongsTo(Resident::class);
+    }
+
+    public function equipment()
+    {
+        return $this->belongsTo(Equipment::class);
+    }
 }
---
+++ app/Nova/TakenEquipment.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class TakenEquipment extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\TakenEquipment::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The relationship columns that should be searched.
+     *
+     * @var array
+     */
+    public static $searchRelations = [
+        'resident.user' => ['firstname', 'lastname'],
+        'equipment' => ['name'],
+    ];
+
+    /**
+     * The relationships that should be eager loaded on index queries.
+     *
+     * @var array
+     */
+    public static $with = ['resident', 'equipment'];
+
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Accommodation';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            BelongsTo::make('resident')->required(),
+            BelongsTo::make('equipment')->required(),
+            Number::make('quantity')->required(),
+            Date::make('take_date')->required(),
+            Date::make('return_date')->nullable()
+        ];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
